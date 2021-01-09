dbf9be15dde46dd34f1db8cab2b4a57d059907e1
weeidl <weeidl@list.ru>
2021-01-09 21:43:44 +0300

    Oasis

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,160 @@
 import 'package:flutter/material.dart';
+import 'widgets/navbar.dart';
+import 'utils/responsiveLayout.dart';
+import 'widgets/search.dart';
 
-void main() {
-  runApp(MaterialApp(
-    MyApp(),
-  ));
-}
+void main() => runApp(MaterialApp(
+      title: 'Flutter Landing Page',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    ));
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(children: [
-        LeftPage(),
-      ]),
-    );
-  }
-}
-
-class LeftPage extends StatefulWidget {
-  @override
-  _LeftPageState createState() => _LeftPageState();
-}
-
-class _LeftPageState extends State<LeftPage> {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(
-              "https://cdn-nus-1.pinme.ru/photo/f5/2b/f52bc20153ee86d94765a015c7cabb13.jpg"),
-          fit: BoxFit.cover,
+          gradient: LinearGradient(colors: [
+        Color(0xFFF8FBFF),
+        Color(0xFFFCFDFD),
+      ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[NavBar(), Body()],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveLayout(
+      largeScreen: LargeChild(),
+      smallScreen: SmallChild(),
+    );
+  }
+}
+
+class LargeChild extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 600,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          FractionallySizedBox(
+            alignment: Alignment.centerRight,
+            widthFactor: .6,
+            child: Image.network("assets/image_01.png", scale: .85),
+          ),
+          FractionallySizedBox(
+            alignment: Alignment.centerLeft,
+            widthFactor: .6,
+            child: Padding(
+              padding: EdgeInsets.only(left: 48),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Hello!",
+                      style: TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Montserrat-Regular",
+                          color: Color(0xFF8591B0))),
+                  RichText(
+                    text: TextSpan(
+                        text: "WellCome To ",
+                        style:
+                            TextStyle(fontSize: 60, color: Color(0xFF8591B0)),
+                        children: [
+                          TextSpan(
+                              text: "Oasis",
+                              style: TextStyle(
+                                  fontSize: 60,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black87))
+                        ]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0, top: 20),
+                    child: Text("LET’S EXPLORE THE WORLD"),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Search()
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SmallChild extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Hello!",
+              style: TextStyle(
+                  fontSize: 40,
+                  color: Color(0xFF8591B0),
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Montserrat-Regular"),
+            ),
+            RichText(
+              text: TextSpan(
+                text: 'WellCome To ',
+                style: TextStyle(fontSize: 40, color: Color(0xFF8591B0)),
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'Britu',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
+                          color: Colors.black87)),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0, top: 20),
+              child: Text("LET’S EXPLORE THE WORLD"),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Image.network(
+                "assets/image_01.png",
+                scale: 1,
+              ),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            Search(),
+            SizedBox(
+              height: 30,
+            )
+          ],
         ),
       ),
     );
